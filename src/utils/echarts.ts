c910737125eed256e3b08d1bad65677a86189655
yangzi <1131696774@qq.com>
2023-10-10 16:51:09 +0800

    feat(Echart): 🚀 添加Echart图表

---
+++ src/utils/echarts.ts
@@ -0,0 +1,60 @@
+// 按需引入
+import * as echarts from "echarts/core";
+import { BarChart, LineChart, PieChart } from "echarts/charts";
+import {
+	TitleComponent,
+	TooltipComponent,
+	GridComponent,
+	// 数据集组件
+	DatasetComponent,
+	// 内置数据转换器组件 (filter, sort)
+	TransformComponent,
+	LegendComponent
+} from "echarts/components";
+import { LabelLayout, UniversalTransition } from "echarts/features";
+import { CanvasRenderer } from "echarts/renderers";
+import type {
+	// 系列类型的定义后缀都为 SeriesOption
+	BarSeriesOption,
+	LineSeriesOption,
+	PieSeriesOption
+} from "echarts/charts";
+import type {
+	// 组件类型的定义后缀都为 ComponentOption
+	TitleComponentOption,
+	TooltipComponentOption,
+	GridComponentOption,
+	DatasetComponentOption,
+	LegendComponentOption
+} from "echarts/components";
+import type { ComposeOption } from "echarts/core";
+
+// 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+export type ECOption = ComposeOption<
+	| BarSeriesOption
+	| LineSeriesOption
+	| PieSeriesOption
+	| TitleComponentOption
+	| TooltipComponentOption
+	| GridComponentOption
+	| DatasetComponentOption
+	| LegendComponentOption
+>;
+
+// 注册必须的组件
+echarts.use([
+	TitleComponent,
+	TooltipComponent,
+	GridComponent,
+	DatasetComponent,
+	TransformComponent,
+	LegendComponent,
+	BarChart,
+	PieChart,
+	LineChart,
+	LabelLayout,
+	UniversalTransition,
+	CanvasRenderer
+]);
+
+export default echarts;
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { Card } from "antd";
 
+import echarts from "@/utils/echarts";
+import type { ECOption } from "@/utils/echarts";
+import "./index.less";
 function Dashboard() {
-	return <div>dashboard</div>;
+	let pieChart = useRef<HTMLDivElement>(null);
+
+	let option: ECOption = {
+		tooltip: {
+			trigger: "item"
+		},
+		legend: {
+			top: "5%",
+			left: "center"
+		},
+		series: [
+			{
+				name: "Access From",
+				type: "pie",
+				radius: ["40%", "70%"],
+				avoidLabelOverlap: false,
+				itemStyle: {
+					borderRadius: 10,
+					borderColor: "#fff",
+					borderWidth: 2
+				},
+				label: {
+					show: false,
+					position: "center"
+				},
+				emphasis: {
+					label: {
+						show: true,
+						fontSize: 40,
+						fontWeight: "bold"
+					}
+				},
+				labelLine: {
+					show: false
+				},
+				data: [
+					{ value: 1048, name: "Search Engine" },
+					{ value: 735, name: "Direct" },
+					{ value: 580, name: "Email" },
+					{ value: 484, name: "Union Ads" },
+					{ value: 300, name: "Video Ads" }
+				]
+			}
+		]
+	};
+	useEffect(() => {
+		let myChart = echarts.init(pieChart.current);
+		option && myChart.setOption(option);
+	}, []);
+
+	return (
+		<Card bordered={false}>
+			<div style={{ width: "100%", height: "500px" }} ref={pieChart}></div>
+		</Card>
+	);
 }
 
 export default Dashboard;
