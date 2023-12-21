da5889651ed4d145c0c004a542859a779eea44ae
coder-czy <coder-czy@users.noreply.github.com>
2023-12-21 02:00:00 +0000

    deploy: 2b9feb5504bc9c8752c6a840920b0d4c20c5a291

---
+++ assets/js/404-e2a69219.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-86934c54.js";import{E as e}from"./index-f2636876.js";function s(){return t.jsx(e,{name:"404",title:"Sorry, the page you visited does not exist."})}export{s as default};
